2e50f9effeaed2db20814b5465094e61e8c295ff
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-12-11 18:45:25 +0700

    Add Dice Roller feature with HTML, CSS, and JavaScript implementation

---
+++ Dice-Roller/script.js
@@ -0,0 +1,20 @@
+function rollDice() {
+  const numOfDice = document.getElementById("numOfDice").value;
+  const diceResult = document.getElementById("diceResult");
+  const diceImages = document.getElementById("diceImages");
+  const sumResult = document.getElementById("sumResult");
+  let sum = 0;
+  const values = [];
+  const images = [];
+
+  for (let i = 0; i < numOfDice; i++) {
+    const value = Math.floor(Math.random() * 6) + 1;
+    sum += value;
+    values.push(value);
+    images.push(`<img src="./dice_images/${value}.png" alt=${value}>`);
+  }
+
+  diceResult.innerHTML = `Dice: ${values.join(", ")}`;
+  diceImages.innerHTML = images;
+  sumResult.innerHTML = `Total : ${sum}`;
+}
