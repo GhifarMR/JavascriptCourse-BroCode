9bbc5a370018ebec7f0f1d472bb29f338d6acb7b
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-12-17 11:31:00 +0700

    adds the `updateDay` function to show the name of the current day.

--- Digital-Clock/script.js
+++ Digital-Clock/script.js
@@ -10,5 +10,45 @@ function updateClock() {
   document.getElementById("clock").textContent = timeString;
 }
 
+function updateDay() {
+  const now = new Date();
+  let day = now.getDay();
+
+  switch (day) {
+    case 0:
+      day = "Sunday";
+      break;
+
+    case 1:
+      day = "Monday";
+      break;
+
+    case 2:
+      day = "Tuesday";
+      break;
+
+    case 3:
+      day = "Wednesday";
+      break;
+
+    case 4:
+      day = "Thursday";
+      break;
+
+    case 5:
+      day = "Friday";
+      break;
+
+    case 6:
+      day = "Saturday";
+      break;
+  }
+
+  document.getElementById("day").textContent = `${day}`;
+}
+
+updateDay();
 updateClock();
+
 setInterval(updateClock, 100);
+setInterval(updateDay, 100);
